9ffe9bbb13b76026aab9684044c1a4b4375d08e9
Carlos Silva <carlos110eduardo@gmail.com>
2024-01-07 15:22:21 -0300

    adicionado arquivo css responsivo e o melhorado o código index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,31 @@
 const botoesSetas = document.querySelectorAll(".seta");
 const respostas = document.querySelectorAll(".resposta");
 
-botoesSetas.forEach(function(botaoSeta, index){
-    botaoSeta.addEventListener("click", function(){
+botoesSetas.forEach(function(botaoSetaAtual, index){
+    botaoSetaAtual.addEventListener("click", function(){
         const respostaAbertaAnterior = document.querySelector('.mostrar-resposta');
+        const respostaAbertaAtual = respostas[index];
+        const botaoSetaAbertoAnterior = document.querySelector("[src='../src/imagens/seta aberta.png']");
 
-        if(respostaAbertaAnterior !== null){
-            respostaAbertaAnterior.classList.remove('mostrar-resposta');
-            respostaAbertaAnterior.classList.add('ocultar-resposta');
+        if(respostaAbertaAnterior === null){
+            respostaAbertaAtual.classList.remove('ocultar-resposta');
+            respostaAbertaAtual.classList.add('mostrar-resposta');
+            botaoSetaAtual.src = '../src/imagens/seta aberta.png';
+            return
         }
-        respostas[index].classList.remove('ocultar-resposta');
-        respostas[index].classList.add('mostrar-resposta');
+        if(respostaAbertaAnterior === respostaAbertaAtual){
+            respostaAbertaAtual.classList.remove('mostrar-resposta');
+            respostaAbertaAtual.classList.add('ocultar-resposta')
+            botaoSetaAtual.src = '../src/imagens/seta fechada.png';
+            return;
+        }
+
+        respostaAbertaAnterior.classList.remove('mostrar-resposta');
+        respostaAbertaAnterior.classList.add('ocultar-resposta');
+        botaoSetaAbertoAnterior.src = '../src/imagens/seta fechada.png';
+        
+        respostaAbertaAtual.classList.remove('ocultar-resposta');
+        respostaAbertaAtual.classList.add('mostrar-resposta')
+        botaoSetaAtual.src = '../src/imagens/seta aberta.png';
     });
 });
