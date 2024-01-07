cbf55556bf6c5c4e09f5b7341dd5d04bd9644b06
Carlos Silva <carlos110eduardo@gmail.com>
2024-01-07 16:01:52 -0300

    corrigido o bug da imagem da seta.

--- src/js/index.js
+++ src/js/index.js
@@ -5,27 +5,27 @@ botoesSetas.forEach(function(botaoSetaAtual, index){
     botaoSetaAtual.addEventListener("click", function(){
         const respostaAbertaAnterior = document.querySelector('.mostrar-resposta');
         const respostaAbertaAtual = respostas[index];
-        const botaoSetaAbertoAnterior = document.querySelector("[src='../src/imagens/seta-aberta.png']");
+        const botaoSetaAbertoAnterior = document.querySelector("[src='./src/imagens/seta-aberta.png']");
 
         if(respostaAbertaAnterior === null){
             respostaAbertaAtual.classList.remove('ocultar-resposta');
             respostaAbertaAtual.classList.add('mostrar-resposta');
-            botaoSetaAtual.src = '../src/imagens/seta-aberta.png';
+            botaoSetaAtual.src = './src/imagens/seta-aberta.png';
             return
         }
         if(respostaAbertaAnterior === respostaAbertaAtual){
             respostaAbertaAtual.classList.remove('mostrar-resposta');
             respostaAbertaAtual.classList.add('ocultar-resposta')
-            botaoSetaAtual.src = '../src/imagens/seta-fechada.png';
+            botaoSetaAtual.src = './src/imagens/seta-fechada.png';
             return;
         }
 
         respostaAbertaAnterior.classList.remove('mostrar-resposta');
         respostaAbertaAnterior.classList.add('ocultar-resposta');
-        botaoSetaAbertoAnterior.src = '../src/imagens/seta-fechada.png';
+        botaoSetaAbertoAnterior.src = './src/imagens/seta-fechada.png';
         
         respostaAbertaAtual.classList.remove('ocultar-resposta');
         respostaAbertaAtual.classList.add('mostrar-resposta')
-        botaoSetaAtual.src = '../src/imagens/seta-aberta.png';
+        botaoSetaAtual.src = './src/imagens/seta-aberta.png';
     });
 });
