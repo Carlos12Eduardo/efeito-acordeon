cc73639c7b848a3dca67db307add195ab5704eea
Carlos Silva <carlos110eduardo@gmail.com>
2024-01-06 22:40:01 -0300

    adicionando arquivo css de responsividade e mehlorado o código js

--- src/js/index.js
+++ src/js/index.js
@@ -1,35 +1,15 @@
 const botoesSetas = document.querySelectorAll(".seta");
 const respostas = document.querySelectorAll(".resposta");
-let respostaAberta = 0;
-botoesSetas.forEach(function(elemento, index){
-    
-    elemento.addEventListener("click", function(){
-        let estaAberto = respostas[index].className.indexOf("mostrar-resposta") > -1;
-        console.log("a resposta está aberta na div: "+respostaAberta);
-        console.log("voce clicou no botao"+index);
-        if (estaAberto) {
-            console.log("tem a classe mostrar-resposta");
-            respostas[respostaAberta].classList.remove("mostrar-resposta");
-            respostas[respostaAberta].classList.add("ocultar-resposta");
-        }else{
-            console.log("não tem a classe mostrar-resposta");
-            respostas[respostaAberta].classList.remove("mostrar-resposta");
-            respostas[respostaAberta].classList.add("ocultar-resposta");
-            respostas[index].classList.remove("ocultar-resposta");
-            respostas[index].classList.add("mostrar-resposta"); 
-            respostaAberta = index;
+
+botoesSetas.forEach(function(botaoSeta, index){
+    botaoSeta.addEventListener("click", function(){
+        const respostaAbertaAnterior = document.querySelector('.mostrar-resposta');
+
+        if(respostaAbertaAnterior !== null){
+            respostaAbertaAnterior.classList.remove('mostrar-resposta');
+            respostaAbertaAnterior.classList.add('ocultar-resposta');
         }
-    } )
+        respostas[index].classList.remove('ocultar-resposta');
+        respostas[index].classList.add('mostrar-resposta');
+    });
 });
-// botoesSetas[0].addEventListener("click", function () {
-//     console.log("voce clicou no botao");
-//     console.log(respostas);
-//     if (respostas[0].className.indexOf("mostrar-resposta") > -1) {
-//         console.log("tem a classe mostrar-resposta");
-//         respostas[0].classList.remove("mostrar-resposta");
-//         respostas[0].classList.add("ocultar-resposta");
-//     }else{
-//         respostas[0].classList.remove("ocultar-resposta");
-//         respostas[0].classList.add("mostrar-resposta"); 
-//     }
-// });
